a78b8471bdd8eb9f363a7346f5fa5e58e27f2d18
S4LM-SEC <salimxhashmix2001@gmail.com>
2025-04-15 22:28:27 +0400

    Add files via upload
    First build , shit still in progress

---
+++ js/commands.js
@@ -0,0 +1,97 @@
+var youtube = "https://www.youtube.com/fknight/";
+var twitter = "https://www.twitter.com/forrestpknight/";
+var password = "fkcodes";
+var linkedin = "https://www.linkedin.com/in/forrestpknight/";
+var instagram = "https://www.instagram.com/forrestpknight/";
+var github = "https://github.com/forrestknight/";
+var email = 'mailto:forrest@fkcodes.com';
+
+whois = [
+  "<br>",
+  "Hey, I'm Forrest!👋",
+  "I'm a software engineer and content creator, who builds engaging websites like this one",
+  "and makes YouTube videos about computer science & software engineering.",
+  "After graduating with a Bachelor's in Computer Science, I worked professionally",
+  "as a software engineer building enterprise web applications for Fortune 500 companies.",
+  "While doing all of that, I documented my coding journey on YouTube - trying to enlighten",
+  "the next generation of developers and help them navigate the crazy world that is software", "engineering & computer science.",
+  "Before I knew it, that online presence took on a life of its own, to the point where I knew",
+  "I needed to make the jump from software engineering to full time content creation - strike while",
+  "the iron's hot.",
+  "Now, I make videos about software engineering and computer science, building anything my heart desires,",
+  "like this terminal website.",
+  "<br>"
+];
+
+whoami = [
+  "<br>",
+  "The paradox of “Who am I?” is: we never know, but, we constantly find out.",
+  "<br>"
+];
+
+social = [
+  "<br>",
+  'youtube        <a href="' + youtube + '" target="_blank">youtube/fknight' + "</a>",
+  'twitter        <a href="' + twitter + '" target="_blank">twitter/forrestpknight' + '</a>',
+  'linkedin       <a href="' + linkedin + '" target="_blank">linkedin/forrestpknight' + "</a>",
+  'instagram      <a href="' + instagram + '" target="_blank">instagram/forrestpknight' + '</a>',
+  'github         <a href="' + github + '" target="_blank">github/forrestknight' + "</a>",
+  "<br>"
+];
+
+secret = [
+  "<br>",
+  '<span class="command">sudo</span>           Only use if you\'re admin',
+  "<br>"
+];
+
+projects = [
+  "<br>",
+  "Still curating... most projects are offline, on GitHub, or confidential.",
+  "<br>"
+];
+
+help = [
+"<br>",
+'<a class="commandlink" href="#" onclick="commander(\'whois\')">whois</a>          Who is Forrest?',
+'<a class="commandlink" href="#" onclick="commander(\'whoami\')">whoami</a>         Who are you?',
+'<a class="commandlink" href="#" onclick="commander(\'video\')">video</a>          View YouTube videos',
+'<a class="commandlink" href="#" onclick="commander(\'social\')">social</a>         Display social networks',
+'<a class="commandlink" href="#" onclick="commander(\'secret\')">secret</a>         Find the password',
+'<a class="commandlink" href="#" onclick="commander(\'projects\')">projects</a>       View coding projects',
+'<a class="commandlink" href="#" onclick="commander(\'history\')">history</a>        View command history',
+'<a class="commandlink" href="#" onclick="commander(\'help\')">help</a>           You obviously already know what this does',
+'<a class="commandlink" href="#" onclick="commander(\'email\')">email</a>          Do not email me',
+'<a class="commandlink" href="#" onclick="commander(\'clear\')">clear</a>          Clear terminal',
+'<a class="commandlink" href="#" onclick="commander(\'banner\')">banner</a>         Display the header',
+"<br>",
+];
+
+banner = [
+  '<span class="index">ForrestKnight (FK) Not A Corporation. All knights reserved.</span>',
+  "         ___   ____",
+  "       /' --;^/ ,-_\\     \\ | /       ",
+  "      / / --o\\ o-\\ \\\\   --(_)--   ",
+  "     /-/-/|o|-|\\-\\\\|\\\\   / | \\   ______                          __     __ __       _       __    __   ",
+  "      '`  ` |-|   `` '          / ____/___  _____________  _____/ /_   / //_/____  (_)___ _/ /_  / /_ ",
+  "            |-|                / /_  / __ \\/ ___/ ___/ _ \\/ ___/ __/  / ,<  / __ \\/ / __ `/ __ \\/ __/ ",
+  "            |-|O              / __/ / /_/ / /  / /  /  __(__  ) /_   / /| |/ / / / / /_/ / / / / /_  ",
+  "            |-(\\,__          /_/    \\____/_/  /_/   \\___/____/\\__/  /_/ |_/_/ /_/_/\\__, /_/ /_/\\__/ ",
+  "         ...|-|\\--,\\_....                                                         /____/    © 2022",
+  "      ,;;;;;;;;;;;;;;;;;;;;;;;;,.   ",
+  "~~,;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;,~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~",
+  '<span class="color2">Welcome to my interactive web terminal.</span>',
+"<br>",
+'<a class="commandlink" href="#" onclick="commander(\'whois\')">whois</a>          Who is Forrest?',
+'<a class="commandlink" href="#" onclick="commander(\'whoami\')">whoami</a>         Who are you?',
+'<a class="commandlink" href="#" onclick="commander(\'video\')">video</a>          View YouTube videos',
+'<a class="commandlink" href="#" onclick="commander(\'social\')">social</a>         Display social networks',
+'<a class="commandlink" href="#" onclick="commander(\'secret\')">secret</a>         Find the password',
+'<a class="commandlink" href="#" onclick="commander(\'projects\')">projects</a>       View coding projects',
+'<a class="commandlink" href="#" onclick="commander(\'history\')">history</a>        View command history',
+'<a class="commandlink" href="#" onclick="commander(\'help\')">help</a>           You obviously already know what this does',
+'<a class="commandlink" href="#" onclick="commander(\'email\')">email</a>          Do not email me',
+'<a class="commandlink" href="#" onclick="commander(\'clear\')">clear</a>          Clear terminal',
+'<a class="commandlink" href="#" onclick="commander(\'banner\')">banner</a>         Display the header',
+"<br>",
+];
